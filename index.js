fbcac21241d1d588f2952c3aeb2a4cb5e657595d
Nic Peterson <nicpeterson@Nics-MacBook-Pro-2.local>
2020-12-27 08:13:07 -0800

    Updated eslint rules to accommodate babel. Was getting an error from ...

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 const usersRepo = require("./repositories/users");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
+app.use(
+  cookieSession({
+    keys: ["LqDzBjFZeQ2v58d9"],
+  })
+);
+
+app.get("/signup", (req, res) => {
   res.send(`
     <div>
+        Your ID is: ${req.session.userId}
         <form method="POST">
             <input name="email" placeholder="email" />
             <input name="password" placeholder="password"/>
@@ -18,7 +26,7 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
   // data is an array of raw info
   const { email, password, passwordConfirmation } = req.body;
 
@@ -27,7 +35,38 @@ app.post("/", async (req, res) => {
   if (password !== passwordConfirmation)
     return res.send("Passwords must match!!");
 
+  const user = await usersRepo.create({ email, password });
+
+  // store the id of that user inside the users cookie
+  req.session.userId = user.id;
+
   res.send("Account created!!!");
+});
+
+app.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("You are logged out!");
+});
+
+app.get("/signin", (req, res) => {
+  res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password"/>
+            <button>Sign In</button>
+        </form>
+    </div>
+    `);
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await usersRepo.getOneBy({ email });
+  if (!user) return res.send("Email not found!");
+  if (user.password != password) return res.send("Invalid password!");
+  req.session.userId = user.id;
+  res.send("You are signed in!");
 });
 
 app.listen(3000, () => {
--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const crypto = require("crypto");
+const util = require("util");
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRespository {
   constructor(filename) {
@@ -25,12 +28,20 @@ class UsersRespository {
   }
 
   async create(attrs) {
-    // {email: '', password: ''}
+    // attrs === {email: '', password: ''}
     attrs.id = this.randomId();
+
+    const salt = crypto.randomBytes(8).toString("hex");
+    const buf = await scrypt(attrs.password, salt, 64);
+
     const records = await this.getAll();
-    records.push(attrs);
+    records.push({
+      ...attrs,
+      password: `${buf.toString("hex")}.${salt}`,
+    });
     await this.writeAll(records);
     // write the updated 'records' array back to this.filename
+    return attrs;
   }
 
   async writeAll(records) {
