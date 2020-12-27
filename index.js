762d663bcd44f2480bd840da47d67b0abf21d818
Nic Peterson <nicpeterson@Nics-MacBook-Pro-2.local>
2020-12-26 20:14:45 -0800

    Finished setting up express.

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password"/>
+            <input name="passwordConfirmation" placeholder ="password confirmation"/>
+            <button>Sign up</button>
+        </form>
+    </div>
+  `);
+});
+
+app.post("/", (req, res) => {
+  // data is an array of raw info
+  console.log(req.body);
+  res.send("Account created!!!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening...");
+});
